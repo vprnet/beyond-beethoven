9f03aa44ac5d87f3b96342d87218120e7d4c59fe
Matt Parrilla <matthew.parrilla@gmail.com>
2014-03-06 14:24:52 -0500

    Unstaged commits

--- app/static/js/script-ck.js
+++ app/static/js/script-ck.js
@@ -1,11 +1,6 @@
 if (document.createElement('audio').canPlayType) {
     if (!document.createElement('audio').canPlayType('audio/mpeg')) {
-        var all_audio = $('audio');
-        for (var i = 0; i < all_audio.length; i++) {
-            var ogg = all_audio.eq(i).attr('ogg');
-            all_audio.eq(i).attr('src', ogg);
-            all_audio.eq(i).attr('type', 'audio/ogg');
-        }
+        $('div.audio_player').attr('style', 'display: none;');
     }
 }
 
@@ -17,14 +12,25 @@ var transition = function(d) {
 };
 
 $('a.audio_play').click(function() {
-    // Next step: check for other audio playing, pause it!
-    var audio = this.firstChild,
+    var audioLink = $(this),
+        audio = this.firstChild,
         progressBar = $(this).next('div.progress_bar'),
         fullWidth = $(this).parent('div.audio_player').width(),
         duration = progressBar.attr('data'),
         remaining = progressBar.attr('remaining'),
         glyphicon = $(this).children('div.play_btn').children('span.glyphicon');
 
+    if (!audioLink.hasClass('can_play')) {
+        glyphicon.attr('class', 'icon ion-loading-c');
+        audioLink.addClass('can_play');
+    }
+
+    audio.addEventListener('ended', canPlay(audio, progressBar, fullWidth, duration, remaining, glyphicon, audioLink));
+});
+
+
+var canPlay = function(audio, progressBar, fullWidth, duration, remaining, glyphicon, audioLink) {
+    // Next step: check for other audio playing, pause it!
     audio.addEventListener('ended', function () {
         audio.currentTime = 0;
         progressBar.attr("style", "width: 0px;");
@@ -46,7 +52,7 @@ $('a.audio_play').click(function() {
         progressBar.attr("style", transition(remaining));
         glyphicon.attr('class', 'glyphicon glyphicon-pause');
     }
-});
+};
 
 
 /* **********************************************
